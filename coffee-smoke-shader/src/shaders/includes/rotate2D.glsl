e9a4cdb016accf320e1e15a5337550109aedd63c
Parmida <147280021+Psaemiyan@users.noreply.github.com>
2024-08-06 09:27:21 -0700

    Folder added

--- coffee-smoke-shader/src/shaders/includes/rotate2D.glsl
+++
@@ -1,9 +0,0 @@
-// this is a simple rotation formula
-
-vec2 rotate2D(vec2 value, float angle)
-{
-    float s = sin(angle);
-    float c = cos(angle);
-    mat2 m = mat2(c, s, -s, c);
-    return m * value;
-}
